6e1d2d0f61baacb9795b9f481275e54242d826f6
Tarmanjot singh <sitarman@sheridancollege.ca>
2026-04-27 13:14:55 -0400

    at college

---
+++ .github/numberAndMaths.js
@@ -0,0 +1,33 @@
+//numbers and maths in javascript
+
+//addition
+let sum = 10 + 20
+console.log(sum) //30 print hoga
+
+//subtraction
+let difference = 20 - 10
+console.log(difference) //10 print hoga
+
+//multiplication
+let product = 10 * 20
+console.log(product) //200 print hoga
+
+//division
+let quotient = 20 / 10
+console.log(quotient) //2 print hoga
+
+//modulus
+let remainder = 20 % 3
+console.log(remainder) //2 print hoga      
+//exponentiation
+let power = 2 ** 3
+console.log(power) //8 print hoga
+
+//increment
+let x = 10
+x++
+console.log(x) //11 print hoga
+
+//decrement
+x--
+console.log(x) //10 print hoga
