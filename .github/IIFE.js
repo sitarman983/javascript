c74699f43ab5cd9dd946e5fd2fd9183b8947b15e
Tarmanjot singh <sitarman@sheridancollege.ca>
2026-04-30 01:48:32 +0000

    iffy

--- .github/IIFE.js
+++ .github/IIFE.js
@@ -1,4 +1,5 @@
 //learning iife
+//we use it to stop poulting the global namespace and to create a new scope for our variables and functions.
 (function() {
     console.log("This is an IIFE");
 })();
@@ -34,3 +35,38 @@ const result = (function() {
 })();
 console.log(result); 
 // This will log "This is an IIFE" to the console, as the IIFE returns a string that is assigned to the variable 'result'.
+
+(function chai()  {
+    console.log(`db connected`);
+})();               // This IIFE will log "db connected" to the console immediately when defined.
+//  ^ semicolons paa lyi nhita ehne donne functions jorh lene and error thro krdena
+//to end function nd start new one ,semicolons zaruri haan, otherwise it will throw an error by
+//  treating them as a single statement.
+
+
+(function IamCoder(){
+    //        ^      jehde function da ehda name rkhya ove ohnu name IIFY kehnde haan, but it is not necessary to give a name to an IIFE,
+    console.log(`i am a coder`);
+})();// This IIFE will log "i am a coder" to the console immediately when defined.
+
+
+
+//to write it as arrow function
+(() =>{
+    //unnammed iffy
+    console.log(`This is an IIFE written as an arrow function`);
+
+})();
+
+
+// ((AnyVariableT) =>{
+//     console.log(`This is an IIFE written as an arrow function${AnyVariableT}`);
+// })("tarman");
+   //^name pass kita AnyVariableT ch te usnu console.log ch use kita,
+   //  asi IIFE nu parameter v pass kar sakde haan, 
+   // te usnu function de andar use kar sakde haan.
+
+   ((AnyVariableT) =>{
+    //unnamed iffy with parameter
+    console.log(`This is an IIFE written as an arrow function${AnyVariableT}`);
+})("tarman");
